d364122b5c44c247bbace247928ad0395820d64d
goolpe <gooolpe@gmail.com>
2018-10-25 15:08:57 +0300

    create separate component homepage for ablitiy to scale application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import HomePage from './components/HomePage';
 
 class App extends Component {
   render(){
     return(
-      <div>App</div>
+      <HomePage />
     )
   }
 }
