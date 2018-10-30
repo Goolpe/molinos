e48d3e81f3b1a8c6b06fc364f1358b631a958384
goolpe <gooolpe@gmail.com>
2018-10-30 19:07:38 +0300

    delete container flex colum in footer - need row

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ function Footer(props) {
   return (
     <footer className='footer'>
       <section className='container container__flex'>
-        <div className='container__flex container__flex--block'>
+        <div className='container__flex--block'>
           <label for='search'>
             <input name='search' id='search' className='container__flex--search__input' placeholder='Поиск' />
           </label>
           <img className='container__flex--apple-icon' alt='appstore' src='/assets/appstore.png' />
         </div>
-        <div className='container__flex container__flex--block'>
+        <div className='container__flex--block'>
           <nav><h4>Редакция  Реклама  Пресс-релизы  Техподдержка  Спецпроекты  Вакансии  RSS</h4></nav>
           <h4>© 1999–2018 ООО «Мировые новости»</h4>
         </div>
